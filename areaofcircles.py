2b91c609c446978b3cf291a6a97cbcfda94bc6c9
Abdishakur-prog <shakraa207@gmail.com>
2024-10-14 17:06:27 +0300

    first commit

---
+++ areaofcircles.py
@@ -0,0 +1,4 @@
+radius=10
+pi=3.147
+area=pi*radius
+print(area)
---
+++ intro.py
@@ -0,0 +1,34 @@
+
+
+# variables in python;
+# = containers used to store data;
+# = Declaration of variables;
+# 1.name of the variable, value being stored.
+
+fname=("Mohamed")
+fname=("techcamp")
+print(fname)
+ 
+#write a program that adds 2 variables num1 and num2 and stores them in a variable called sum
+
+num1=10
+num2=20
+sum=num1+num2
+print(num1 + num2)
+
+# declare two variables fname=your fname,lname add them up to get full name
+
+fname=("Abdishakur")
+sname=("Mohamed")
+full_name=fname+" "+sname
+print(full_name)
+
+
+
+#input function; takes input from the terminal
+phone_number=input("Enter phone number"+" ")
+print(phone_number)
+
+
+name=input("Enter your name"+" ")
+print("Hello"+" "+name)
