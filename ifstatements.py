9bd52b32155e9370be15e6aa2125dfdec4039bd8
Abdishakur-prog <shakraa207@gmail.com>
2024-10-28 15:50:22 +0300

    update

--- ifstatements.py
+++ ifstatements.py
@@ -11,99 +11,62 @@ if num%2==0:
 else:
     print("odd")
 
-# num1=int(input("enter number: "))
-# num2=int(input("enter number: "))
-# num3=int(input("enter number: "))
-
-# if num1>num2 and num1>num3:
-#     print("num1 is greater")
-
-# elif num2>num1 and num2>num3:
-#     print("num2 is greater")
- 
-# else:
-#     print("num 3 is greater")
 
 
-# temp=int(input("enter temp: "))
+marks=int(input("enter marks: "))
+if marks>=90 and marks<=100:
+  print("A")
+elif marks>=80 and marks<=90:
+  print("B")
+elif marks>=70 and marks<=80:
+  print("C")
+elif marks>=60 and marks<=70:
+  print("D")
+elif marks>=50 and marks<=60:
+  print("E")
+else:
+    print("fail")
 
-# if temp>=30:
-#     print("the temp is too high")
+age=int(input("enter age: "))  
+if age>=60:
+  print("older")
+elif age>18 and age<=60:
+  print("adult")
+else:
+   print("minor")
 
-# elif temp>=15 and temp<=30:
-#     print("normal temp")
+#nested if statements
+purchase=int(input("Enter amount: "))
+if purchase>=1000:
+  print("100sh discount")
+  if purchase>=5000:
+    print("200sh discount")
+else:
+      print("no discount")
 
-# else:
-#     print("cold temp")
-
-
-# x=int(input("enter number: "))
-# y=int(input("enter number: "))
-# if x>=10 and x<=20 and y>=100:
-#  print("conditon met")
-# else:
-#  print("condition not met")
-
-
-# marks=int(input("enter marks: "))
-# if marks>=90 and marks<=100:
-#   print("A")
-# elif marks>=80 and marks<=90:
-#   print("B")
-# elif marks>=70 and marks<=80:
-#   print("C")
-# elif marks>=60 and marks<=70:
-#   print("D")
-# elif marks>=50 and marks<=60:
-#   print("E")
-# else:
-#     print("fail")
-
-# age=int(input("enter age: "))  
-# if age>=60:
-#   print("older")
-# elif age>18 and age<=60:
-#   print("adult")
-# else:
-#    print("minor")
-
-# #nested if statements
-# purchase=int(input("Enter amount: "))
-# if purchase>=1000:
-#   print("100sh discount")
-#   if purchase>=5000:
-#     print("200sh discount")
-# else:
-#       print("no discount")
-
-# age1=int(input("enter your age: "))
-# if age1>=18:
-#   licenses=input("do you have a license yes/no: ").lower().strip()
-#   if licenses=="yes":
-#     print("eligible to drive")
-#   else:
-#     print("you are not eligible to drive")
-# else:
-#   print("you are to young to drive")
+age1=int(input("enter your age: "))
+if age1>=18:
+  licenses=input("do you have a license yes/no: ").lower().strip()
+  if licenses=="yes":
+    print("eligible to drive")
+  else:
+    print("you are not eligible to drive")
+else:
+  print("you are to young to drive")
 
 
-# # Write a program that:
-# # =>Takes the user's credit score and annual income as input.
-# # =>If the credit score is above 700, check if the income is above $50,000:
-# # =>If both conditions are met, print "Loan approved."
-# # =>If only the credit score is high, print "Income requirement not met."
-# # =>If the credit score is below 700, print "Credit score too low."
+# Write a program that:
+# =>Takes the user's credit score and annual income as input.
+# =>If the credit score is above 700, check if the income is above $50,000:
+# =>If both conditions are met, print "Loan approved."
+# =>If only the credit score is high, print "Income requirement not met."
+# =>If the credit score is below 700, print "Credit score too low."
 
 
 credit_score=int(input("Enter credit score: "))
 annual_income=float(input("Enter annual income: "))
 
-# if credit_score>700 and annual_income>=50000:
-#   print("loan approaved")
-#   if credit_score>700 and annual_income<50000:
-#     print("income required not met")
-# else:
-#   print("credit score too low")
+
   
 if credit_score>700:
   if annual_income>50000:
@@ -117,4 +80,6 @@ else:
 
 
 
- 
+
+
+
---
+++ ifstatements_tasks.py
@@ -0,0 +1,91 @@
+# Take three inputs from a user, separately. Print the largest of the numbers.
+#     Hint: Determine what type of data is taken in as input.
+# Take as input from a user the temperature if the temperature is above 30°C display “The temperature is too high”,if the temperature is above 15 display “Normal temperature” otherwise display “Cold temperature”
+# 3.	Write a Python program that checks if a variable x is between 10 and 20 (inclusive)
+# and if another variable y is greater than 100. If both conditions are true, print "Conditions met", otherwise print "Conditions not met"
+# 4. Write a Python program that checks if a variable password is equal to the string "secret123". If it is, print "Access   granted", otherwise print "Access denied"
+# 5. Write a Python program that checks if a variable student_score is greater than 90. If true, check if the attendance is greater than 80. If both conditions are true, print "Excellent student", otherwise print "Good score, but attendance needs improvement"
+# Write a program that:
+# Takes a transaction amount and account type ("Standard" or "Premium") as input.
+# If the account type is "Standard":
+# Check if the amount is above 500:
+# If it is, print "Transaction exceeds the limit for Standard accounts."
+# If not, print "Transaction approved."
+# If the account type is "Premium":
+# Check if the amount is above 1,000:
+# If it is, print "Transaction exceeds the limit for Premium accounts."
+# If not, print "Transaction approved."
+
+#Q1
+num1=int(input("enter number: "))
+num2=int(input("enter number: "))
+num3=int(input("enter number: "))
+
+if num1>num2 and num1>num3:
+    print("num1 is greater")
+
+elif num2>num1 and num2>num3:
+    print("num2 is greater")
+ 
+else:
+    print("num 3 is greater")
+
+#Q2
+temp=int(input("enter temp: "))
+
+if temp>=30:
+    print("the temp is too high")
+
+elif temp>=15 and temp<=30:
+    print("normal temp")
+
+else:
+    print("cold temp")
+
+#Q3
+x=int(input("enter number: "))
+y=int(input("enter number: "))
+if x>=10 and x<=20:
+    if y>100:
+      print("conditon met")
+else:
+ print("condition not met")
+
+
+#Q4
+password=input("enter password: ")
+correct_password="secret123"
+if password=="secret123":
+    print("Access granted")
+else:
+    print("Access denied")
+
+#Q5 
+student_score=int(input("Enter score: "))
+attendance=int(input("enter attendance: "))
+
+if student_score>90:
+    if attendance>80:
+        print("Excellent student")
+    else:
+        print("Good score, but attendance needs improvement")
+else:
+ print("improvement")
+
+
+#Q6 
+amount=float(input("Enter amount: "))
+account_type=(input("Enter account type standard/permium: ")).lower().strip()
+
+if account_type=="standard":
+    if amount>500:
+        print("Transaction exceeds the limit for Standard accounts")
+    else:
+        print("Transaction approved")
+elif account_type=="premium":
+    if amount>1000:
+        print("Transaction exceeds the limit for Standard accounts")
+    else:
+        print("Transaction approved")
+else:
+    print("invalid account type")
